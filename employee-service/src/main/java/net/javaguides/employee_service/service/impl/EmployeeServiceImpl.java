26595b28379ee56409ec79873ea60999f203b1a8
chrisann.lee <chrisannlee97@gmail.com>
2025-06-27 21:52:10 +0800

    microservices communication using WebClient

--- employee-service/src/main/java/net/javaguides/employee_service/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/javaguides/employee_service/service/impl/EmployeeServiceImpl.java
@@ -12,17 +12,19 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final ModelMapper modelMapper;
-    private final RestTemplate restTemplate;
+    //private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository, ModelMapper modelMapper, RestTemplate restTemplate) {
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository, ModelMapper modelMapper, WebClient webClient) {
         this.employeeRepository = employeeRepository;
         this.modelMapper = modelMapper;
-        this.restTemplate = restTemplate;
+        this.webClient = webClient;
     }
 
     @Override
@@ -35,14 +37,34 @@ public class EmployeeServiceImpl implements EmployeeService {
         return modelMapper.map(savedEmployee, EmployeeDto.class);
     }
 
+//    Microservice using RestTemplate
+//    @Override
+//    public APIResponseDto getEmployeeById(Long id) {
+//        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee", "id", id));
+//        EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
+//        //EmployeeDto employeeDto = AutoEmployeeMapper.MAPPER.mapToEmployeeDto(employee);
+//
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
+//        DepartmentDto departmentDto = responseEntity.getBody();
+//
+//        APIResponseDto apiResponseDto = new APIResponseDto();
+//        apiResponseDto.setEmployeeDto(employeeDto);
+//        apiResponseDto.setDepartmentDto(departmentDto);
+//        return apiResponseDto;
+//    }
+
+    //    Microservice using WebClient
     @Override
     public APIResponseDto getEmployeeById(Long id) {
         Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee", "id", id));
         EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
         //EmployeeDto employeeDto = AutoEmployeeMapper.MAPPER.mapToEmployeeDto(employee);
 
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
-        DepartmentDto departmentDto = responseEntity.getBody();
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
         APIResponseDto apiResponseDto = new APIResponseDto();
         apiResponseDto.setEmployeeDto(employeeDto);
