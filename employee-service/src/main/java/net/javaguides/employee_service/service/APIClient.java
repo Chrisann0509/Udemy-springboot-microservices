88031ac70665ece34c999f9167e34d0872bb7c5e
chrisann.lee <chrisannlee97@gmail.com>
2025-08-24 22:54:28 +0800

    Load Balancing with Eureka, Open Feign and Spring Cloud LoadBalancer

--- employee-service/src/main/java/net/javaguides/employee_service/service/APIClient.java
+++ employee-service/src/main/java/net/javaguides/employee_service/service/APIClient.java
@@ -6,7 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+//Use service id can help to use the load balancer to call the rest API dynamically (use available port)
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartmentByCode(@PathVariable("department-code") String departmentCode) ;
