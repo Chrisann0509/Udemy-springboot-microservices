c83e45b604c2490348e2f6c3e83080f2078a338a
chrisann.lee <chrisannlee97@gmail.com>
2025-12-30 17:07:22 +0800

    Swagger API Documentation: Customizing Swagger API Documentation using Annotation

--- department-service/src/main/java/net/javaguides/department_service/controller/DepartmentController.java
+++ department-service/src/main/java/net/javaguides/department_service/controller/DepartmentController.java
@@ -1,11 +1,18 @@
 package net.javaguides.department_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import net.javaguides.department_service.dto.DepartmentDto;
 import net.javaguides.department_service.service.DepartmentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Department Service - Department Controller",
+        description = "Department Controller Exposes APIs for Department Service"
+)
 @RestController
 @RequestMapping("api/departments")
 public class DepartmentController {
@@ -15,6 +22,14 @@ public class DepartmentController {
         this.departmentService = departmentService;
     }
 
+    @Operation(
+            summary = "Save Department REST API",
+            description = "Save Department REST API is used to save department object in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
         DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
@@ -22,6 +37,14 @@ public class DepartmentController {
         return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Department REST API",
+            description = "Get Department REST API is used to get department object from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("{department-code}")
     public ResponseEntity<DepartmentDto> getDepartmentByCode(@PathVariable("department-code") String departmentCode) {
         DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
--- department-service/src/main/java/net/javaguides/department_service/dto/DepartmentDto.java
+++ department-service/src/main/java/net/javaguides/department_service/dto/DepartmentDto.java
@@ -1,17 +1,30 @@
 package net.javaguides.department_service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "DepartmentDto Modal Information"
+)
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class DepartmentDto {
     private Long id;
+    @Schema(
+            description = "Department Name"
+    )
     private String departmentName;
+    @Schema(
+            description = "Department Description"
+    )
     private String departmentDescription;
+    @Schema(
+            description = "Department Code"
+    )
     private String departmentCode;
 }
