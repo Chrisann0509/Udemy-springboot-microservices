cb3e742c8a9a337ec63e96ae38732d8f76d9497f
chrisann.lee <chrisannlee97@gmail.com>
2025-12-30 16:52:42 +0800

    Swagger API Documentation: Define General API Information using Annotations

--- department-service/src/main/java/net/javaguides/department_service/DepartmentServiceApplication.java
+++ department-service/src/main/java/net/javaguides/department_service/DepartmentServiceApplication.java
@@ -1,11 +1,36 @@
 package net.javaguides.department_service;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Department Service REST APIs",
+                description = "Department Service REST API Documentation",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "name",
+                        email = "email@gamil.com",
+                        url = "https://www.javaguides.net"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.javaguides.net"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Department Service Doc",
+                url = "https://www.javaguides.net"
+        )
+)
 public class DepartmentServiceApplication {
     @Bean
     public ModelMapper modelMapper() {
